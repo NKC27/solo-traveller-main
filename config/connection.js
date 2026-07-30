4bce720e03f30175c2a9197eec639956e696dbfb
NKC27 <93439579+NKC27@users.noreply.github.com>
2026-07-30 09:45:34 +0100

    Update database connection for production deployment

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.MYSQL_URL) {
-  sequelize = new Sequelize(process.env.MYSQL_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST || '127.0.0.1',
-      dialect: 'mysql',
-      port: process.env.DB_PORT || 3306,
-    },
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+  },
+);
 
 module.exports = sequelize;
